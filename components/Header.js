afb8fd4bb5d38a20da6609bcdfdd234ef7d5e028
Xin Deng <xind1470@gmail.com>
2024-02-27 13:05:49 -0800

    added header component

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+  <>
+      <div className="w-full"></div>
+      <header className="flex items-center h-12 text-4xl pt-8 pb-8 pl-4 bg-green-500">
+      <h1>Cookie Stand Admin</h1>
+      </header>
+  </>
+  )
+}
