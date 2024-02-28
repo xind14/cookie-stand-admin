406ef1332352cfb5f7359e5fb380f4788816cfa2
Xin Deng <xind1470@gmail.com>
2024-02-28 13:44:45 -0800

    added overview and home button to header

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,14 @@
+import Link from 'next/link';
+
+
 export default function Header() {
   return (
   <>
       <div className="w-full"></div>
-      <header className="flex items-center h-12 text-4xl pt-8 pb-8 pl-4 bg-green-500">
+      <header className="flex items-center justify-between h-12 text-4xl pt-8 pb-8 pl-4 bg-green-500">
       <h1>Cookie Stand Admin</h1>
+      <Link href='/overview' className='px-2 mx-4 rounded-md bg-white text-base'>Overview</Link>
+
       </header>
   </>
   )
