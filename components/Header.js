5fdb7205aae11668ad9576747b4f536b41b620e1
Xin Deng <xind1470@gmail.com>
2024-02-29 19:25:55 -0800

    added user and sign out button

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,19 @@
 import Link from 'next/link';
 
-
-export default function Header() {
+export default function Header({user, logout}) {
   return (
   <>
-      <div className="w-full"></div>
-      <header className="flex items-center justify-between h-12 text-4xl pt-8 pb-8 pl-4 bg-green-500">
+     <header className="flex items-center justify-between h-12 text-4xl pt-8 pb-8 pl-4 bg-green-500">
       <h1>Cookie Stand Admin</h1>
-      <Link href='/overview' className='px-2 mx-4 rounded-md bg-white text-base'>Overview</Link>
-
+      <div className='flex'>
+        {user && (
+          <>
+            <button className='px-2 mx-4 rounded-md bg-white text-base'>{user.username}</button>
+            <button className=' text-base px-2 mx-4 rounded-md bg-green-700 text-white' onClick={logout}>Sign Out</button>
+            <Link href='/overview' className='px-2 mx-4 rounded-md bg-white text-base'>Overview</Link>
+          </>
+        )}
+      </div>
       </header>
   </>
   )
